c34ed8a6baf0eb30919a79ccba304b200e0ef3ae
Artem Surzhko <a_surzhko@bk.ru>
2024-01-25 11:24:34 +0300

    added some change

--- src/components/pages/Projects/ProjectSliderAcc.tsx
+++ src/components/pages/Projects/ProjectSliderAcc.tsx
@@ -46,7 +46,7 @@ function ProjectSliderAcc() {
 
   const navigate = useNavigate;
   const [content, setContent] = useState(contentItems);
-  const [openId, setOpenId] = useState(0);
+  const [openId, setOpenId] = useState(999999);
 
   /* useEffect(() => {
     const lastIndex = content.length - 1;
@@ -61,11 +61,11 @@ function ProjectSliderAcc() {
   return (
     <>
       <div className="flex w-[95%] h-[38rem] justify-center items-center -mt-[4rem] text-white  mx-auto">
-        <ul className="accordion-hr min-h-[300px] list-none flex w-full h-full px-[2rem] pt-[4rem] flex-row  gap-2">
+        <ul className="min-h-[300px] list-none flex w-full h-full px-[2rem] pt-[4rem] flex-row justify-center gap-8">
           {content.map((items: any, id: number) => {
             const { topic, name, desc } = items;
 
-            const isOpen = id === openId;
+            let isOpen = id === openId;
 
             const length = contentItems.length;
 
@@ -81,35 +81,42 @@ function ProjectSliderAcc() {
             }*/
             return (
               <li
-                className={`accordion-hr-item flex overflow-hidden w-[10rem] h-full  text-left`}
+                className={`accordion-hr-item flex overflow-hidden w-[320px] h-full shadow-xl rounded-xl  text-left`}
                 style={{
-                  width: isOpen ? `${widthWindow - 50 * (length - 1)}px` : "",
+                  width: isOpen ? `${widthWindow - 400 * (length - 1)}px` : "",
                 }}
                 key={id}
               >
                 <div
-                  className=" cursor-pointer relative w-[10rem] shrink-0 accordion-hr-header flex justify-center  text-4xl border-r-2 "
+                  className=" cursor-pointer  w-[320px] shrink-0 accordion-hr-header flex flex-col  p-[2rem] text-4xl "
                   onClick={() => {
                     setOpenId(id);
                   }}
                 >
-                  <h1 className=" block absolute   w-full text-2xl font-thin leading-none origin-left ">
-                    {topic}
-                  </h1>{" "}
-                  <button>
-                    <RxDot />
-                  </button>
+                  <div className="flex items-center flex-col">
+                    <h1 className=" w-full text-2xl font-thin leading-none text-center ">
+                      {topic}
+                    </h1>{" "}
+                    <button className="mt-[2rem]">
+                      <RxDot />
+                    </button>
+                  </div>
+                  <div className="flex items-center justify-center h-full">
+                    <p className="text-xl">
+                      Здесь будет текст описывающий кааждый блок
+                    </p>
+                  </div>
                 </div>
                 <div
-                  className={`accordion-hr-body shrink-0   h-full py-[5rem] px-[4rem] overflow-y-auto overflow-hidden`}
+                  className={`accordion-hr-body shrink-0 h-full p-[1rem]  overflow-y-auto overflow-hidden border-l-2`}
                   style={{
-                    width: isOpen ? `${widthWindow - 200 * length}px` : "",
+                    width: isOpen ? `${widthWindow - 400 * length}px` : "",
                   }}
                 >
                   <h1 className="font-thin tracking-widest text-[1.5rem] leading-relaxed ml-[2rem] mb-[2rem] ">
                     {name.firstName}
                   </h1>
-                  <p className="ml-[2rem] text-xl">{desc}</p>
+                  <p className="ml-[2rem]">{desc}</p>
                 </div>
               </li>
             );
